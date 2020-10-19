c3bb5c9c5a7a19d417c4339a2f61ee31f554979a
Dzmitry Datskevich <dda.enigmer@gmail.com>
2020-10-19 22:29:26 +0300

    fix: change code js

--- js/main.js
+++ js/main.js
@@ -1,102 +1,77 @@
-const time = document.getElementById('time');
-const greeting = document.getElementById('greeting');
-const name = document.getElementById('name');
-const focus = document.getElementById('focus');
+const time = document.getElementById("time");
+const greeting = document.getElementById("greeting");
+const name = document.getElementById("name");
+const focus = document.getElementById("focus");
 
-//функция показывает время 
+//функция показывает время
 function showTime() {
-    let today = new Date();
-    let hour = today.getHours();
-    let min = today.getMinutes();
-    let sec = today.getSeconds();
-    // вывод времени 
-    time.innerHTML = `${hour}:${addZero(min)}:${addZero(sec)}`;
-    // вызываем каждую секунду меняй 
-    setTimeout(showTime, 1000);
+  const today = new Date();
+  const hour = today.getHours();
+  const min = today.getMinutes();
+  const sec = today.getSeconds();
+  // вывод времени
+  time.innerHTML = `${hour}:${addZero(min)}:${addZero(sec)}`;
+  setBgAndTitle(hour);
+  // вызываем каждую секунду меняй
+  setTimeout(showTime, 1000);
 }
 //функция добовления нуля во времени
 function addZero(n) {
-    return (parseInt(n, 10) < 10 ? '0' : '') + n;
+  return (parseInt(n, 10) < 10 ? "0" : "") + n;
 }
 
 // изменение background и текста прветствия
-function setBgname() {
-    let today = new Date();
-    let hour = today.getHours();
-    
-    if (hour < 12) {
-        //утро
-        document.body.style.backgroundImage = "url('../img/1.jpg')";
-        greeting.textContent = "Доброе утро";
-    } else if(hour < 17){
-        // полдень
-        document.body.style.backgroundImage = "url('../img/2.jpg')";
-        greeting.textContent = "Добрый день";
-        document.body.style.color = 'white'; 
-        document.body.style.textShadow = '0 0 20px #000';      
-    } else if(hour < 20){
-            // вечер
-            document.body.style.backgroundImage = "url('../img/3.jpg')";
-            greeting.textContent = "Добрый вечер";
-            document.body.style.color = 'white';
-            document.body.style.textShadow = '0 0 20px #000';
-    } else {
-        // ночь
-        document.body.style.backgroundImage = "url('../img/4.jpg')";
-        greeting.textContent = "Доброй ночи";
-        document.body.style.color = 'white';
-        
-    }
-
+function setBgAndTitle(hour) {
+  if (hour < 12) {
+    //утро
+    document.body.style.backgroundImage = "url('../img/1.jpg')";
+    greeting.textContent = "Доброе утро";
+  } else if (hour < 17) {
+    // полдень
+    document.body.style.backgroundImage = "url('../img/2.jpg')";
+    greeting.textContent = "Добрый день";
+    document.body.style.color = "white";
+    document.body.style.textShadow = "0 0 20px #000";
+  } else if (hour < 20) {
+    // вечер
+    document.body.style.backgroundImage = "url('../img/3.jpg')";
+    greeting.textContent = "Добрый вечер";
+    document.body.style.color = "white";
+    document.body.style.textShadow = "0 0 20px #000";
+  } else {
+    // ночь
+    document.body.style.backgroundImage = "url('../img/4.jpg')";
+    greeting.textContent = "Доброй ночи";
+    document.body.style.color = "white";
+  }
 }
 
 // изменение локальногог хранилища имени
-function getName () {
-    if(localStorage.getItem('name') === null){
-        name.textContent = '[enter your name]';
-    } else {
-        name.textContent = localStorage.getItem('name'); 
-    }
-
+function setValueFromLocalStorage(localStorageId, element) {
+  if (localStorage.getItem(localStorageId) === null) {
+    element.textContent = `[enter your ${localStorageId}]`;
+  } else {
+    element.textContent = localStorage.getItem(localStorageId);
+  }
 }
+
 //записать имя в локальное хранилище
-function setName(e){
-    if (e.type === 'keypress') {
-        if(e.witch == 13 || e.keyCode == 13) {
-            localStorage.setItem('name', e.target.innerText);
-            name.blur();
-        }
-    } else {
-        localStorage.setItem('name', e.target.innerText);
+function setValueToLocalStorage(e, localStorageId) {
+  if (e.type === "keypress") {
+    if (e.witch == 13 || e.keyCode == 13) {
+      localStorage.setItem(localStorageId, e.target.innerText);
+      e.target.blur();
     }
-}
-//записать задачу в локальное хранилище
-function setFocus(e){
-    if (e.type === 'keypress') {
-        if(e.witch == 13 || e.keyCode == 13) {
-            localStorage.setItem('focus', e.target.innerText);
-            focus.blur();
-        }
-    } else {
-        localStorage.setItem('focus', e.target.innerText);
-    }
+  } else {
+    localStorage.setItem(localStorageId, e.target.innerText);
+  }
 }
 
-// изменение локальногог хранилища задачи
-function getFocus () {
-    if(localStorage.getItem('focus') === null){
-        focus.textContent = '[enter your focus]';
-    } else {
-        focus.textContent = localStorage.getItem('focus'); 
-    }
+name.addEventListener("keypress", (e) => setValueToLocalStorage(e, "name"));
+name.addEventListener("blur", (e) => setValueToLocalStorage(e, "name"));
+focus.addEventListener("keypress", (e) => setValueToLocalStorage(e, "focus"));
+focus.addEventListener("blur", (e) => setValueToLocalStorage(e, "focus"));
 
-}
-name.addEventListener('keypress', setName);
-name.addEventListener('blur', setName);
-focus.addEventListener('keypress', setFocus);
-focus.addEventListener('blur', setFocus);
-
-setBgname();
 showTime();
-getName();
-getFocus();
+setValueFromLocalStorage("name", name);
+setValueFromLocalStorage("focus", focus);
